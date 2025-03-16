60451490a66b9bb0d97de9fd03e2a999ceb810f2
MikeMclean-OSU <137909998+MikeMclean-OSU@users.noreply.github.com>
2025-03-16 02:39:40 +0000

    Added Keygen.c

--- keygen.c
+++ keygen.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+int main(int argc, char *argv[]){
+    if (argc < 2){
+        fprintf(stderr, "UASGE: %s characters\n", argv[0]);
+        exit(1);
+    }
+
+    int length = atoi(argv[1]);
+    char *key_file = malloc(length + 1);
+    srand(time(NULL));
+    char random_letter;
+
+    for (int i = 0; i < length; i++){
+        int value = rand() % 27;
+        if (value == 26){
+            random_letter = ' ';
+        }else{
+            random_letter = 'A' + value;
+        }
+        key_file[i] = random_letter;
+    }
+    key_file[length] = '\0';
+    
+    printf("%s\n", key_file);
+
+}
